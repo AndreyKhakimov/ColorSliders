86d55bdd6a5be907a9b07471cea9d51506fe2284
Andrey Khakimov <khakimov013@gmail.com>
2021-10-23 17:11:15 +0300

    Initial Commit

---
+++ ColorSliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSliders
+//
+//  Created by Andrey Khakimov on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
